43e5b3689f6d587af115a35f0f394f40ab83b1ac
Andre Filipe Ferreira Miranda <andrferr@c2r6p10.hive.fi>
2023-03-13 19:51:20 +0200

    ex05 added

---
+++ ex05/Harl.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 18:33:31 by andrferr          #+#    #+#             */
+/*   Updated: 2023/03/13 19:50:19 by andrferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+
+}
+
+Harl::~Harl(void)
+{
+
+}
+
+void	Harl::debug(void)
+{
+	cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << endl;
+}
+
+void	Harl::info(void)
+{
+	cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << endl;
+}
+
+void	Harl::warning(void)
+{
+	cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << endl;
+}
+
+void	Harl::error(void)
+{
+	cout << "This is unacceptable! I want to speak to the manager now." << endl;
+}
+
+void	Harl::complain(string level)
+{
+	string	possible_complains[4] = {
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+
+	void (Harl::*functions[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (possible_complains[i].compare(level) == 0)
+		{
+			fp = functions[i];
+			(this->*fp)();
+			break ;
+		}
+	}
+}
