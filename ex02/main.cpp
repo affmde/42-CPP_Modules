6b33c7a8cf552b94cbd549587269787e7922b586
Andre Filipe Ferreira Miranda <andrferr@c2r6p10.hive.fi>
2023-03-23 16:37:08 +0200

    added more tests to main from ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 12:28:23 by andrferr          #+#    #+#             */
-/*   Updated: 2023/03/21 17:47:14 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/03/23 16:35:40 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,27 @@
 
 int	main(void)
 {
-	Dog a;
-	a.makeSound();
-	a.addIdeaAt(1, "I am a dog");
-	std::cout << a.getIdeaAt(1) << std::endl;
-	Dog b(a);
-	std::cout << "i am B and " << b.getIdeaAt(1) << std::endl;
+	/*Basic Test*/
+	// Dog a;
+	// a.makeSound();
+	// a.addIdeaAt(1, "I am a dog");
+	// std::cout << a.getIdeaAt(1) << std::endl;
+	// Dog b(a);
+	// std::cout << "i am B and " << b.getIdeaAt(1) << std::endl;
+
+	/*Not possible to Instantiate Animal. Should give error*/
+	// AAnimal a;
+	// a.getType();
+
+	/*Test assignemnt operator and copy constructor*/
+	// Dog a;
+	// a.addIdeaAt(0, "I am a dog");
+	// Dog b = a;
+	// Dog c(b);
+	// std::cout << "Dog B: " << b.getIdeaAt(0) << std::endl;
+	// a.addIdeaAt(0, "I am still a dog");
+	// std::cout << "Dog A: " << a.getIdeaAt(0) << std::endl;
+	// std::cout << "Dog B: " << b.getIdeaAt(0) << std::endl;
+	// std::cout << "Dog C: " << c.getIdeaAt(0) << std::endl;
 	return 0;
 }
