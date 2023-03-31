9e57ef3a4f93ff6604b460b67c6cc2865b688fa7
Andre Filipe Ferreira Miranda <andrferr@c2r6p12.hive.fi>
2023-03-31 12:24:27 +0300

    m1 finished

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 16:49:03 by andrferr          #+#    #+#             */
-/*   Updated: 2023/03/29 15:47:19 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/03/31 12:23:22 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(void)
 	std::string &stringREF = str;
 
 	std::cout << "memory adress of str:         " << &str << std::endl;
-	std::cout << "memory adress of stringPTR:   " << &stringPTR << std::endl;
+	std::cout << "memory adress of stringPTR:   " << stringPTR << std::endl;
 	std::cout << "memory adress of &stringREF:  " << &stringREF << std::endl;
 
 	std::cout << "value of str:                 " << str << std::endl;
