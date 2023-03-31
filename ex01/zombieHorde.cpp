2809bae3ee07e4a4d81199d76a1127e9abf53a2f
Andre Filipe Ferreira Miranda <andrferr@c2r6p12.hive.fi>
2023-03-31 13:25:37 +0300

    ex01 fixed

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 16:24:01 by andrferr          #+#    #+#             */
-/*   Updated: 2023/03/14 17:08:35 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/03/31 13:25:19 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ Zombie	*zombieHorde(int N, std::string name)
 	Zombie *horde = new Zombie[N];
 
 	for (int i = 0; i < N; i++)
-		horde[i].SetName(name);
+		horde[i].setName(name);
 	return (horde);
 }
