9e6bfd19150f3d491d9b95f396deb0c14acc04ed
Andre Filipe Ferreira Miranda <andrferr@c2r5p13.hive.fi>
2023-05-11 11:09:57 +0300

    ex00 started. makefile improved

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 17:17:13 by andrferr          #+#    #+#             */
+/*   Updated: 2023/05/10 17:24:34 by andrferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class	ScalarConverter
+{
+	public:
+
+	protected:
+
+	private:
+
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 11:04:24 by andrferr          #+#    #+#             */
+/*   Updated: 2023/05/11 11:09:02 by andrferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	std::cout << "WORKING FINE" << std::endl;
+	return (0);
+}
