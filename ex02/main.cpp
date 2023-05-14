1f106525b68f045bf8eb8756ad004331d1ee5be1
affmde <andrferr@student.hive.fi>
2023-05-14 12:33:45 +0300

    ex02 classes created. generate and pointer identify function done

---
+++ ex02/main.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/14 11:33:17 by andrferr          #+#    #+#             */
+/*   Updated: 2023/05/14 12:33:06 by andrferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <time.h>
+
+Base	*generate(void)
+{
+	Base *base;
+	
+	srand(time(NULL));
+	int	random = rand() %3;
+	switch (random)
+	{
+		case 0:
+			base = new A();
+			break;
+		case 1:
+			base = new B();
+			break;
+		case 2:
+			base = new C();
+			break;
+	}
+	return (base);
+}
+
+void	identify(Base *p)
+{
+	A *a = dynamic_cast<A*>(p);
+	if (a)
+		std::cout << "A" << std::endl;
+	B *b = dynamic_cast<B*>(p);
+		if (b)
+		std::cout << "B" << std::endl;
+	C *c= dynamic_cast<C*>(p);
+	if (c)
+		std::cout << "C" << std::endl;
+}
+
+void	identify(Base &other)
+{
+	
+}
+
+int	main(void)
+{
+	Base *a = generate();
+	identify(a);
+	return (0);
+}
