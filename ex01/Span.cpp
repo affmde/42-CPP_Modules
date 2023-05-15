08024b3f7f1722d71c3df7fc099672103d434d20
affmde <andrferr@student.hive.fi>
2023-05-15 22:32:57 +0300

    ex01 classes and functions created

---
+++ ex01/Span.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 16:43:52 by andrferr          #+#    #+#             */
+/*   Updated: 2023/05/15 21:55:15 by andrferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+Span::Span(unsigned int n)
+{
+	this->_max = n;
+}
+
+Span::Span(const Span &other)
+{
+	this->_max = other._max;
+}
+
+Span	&Span::operator=(const Span &other)
+{
+	this->_max = other._max;
+	return (*this);
+}
+
+Span::~Span(void){}
+
+//Exception
+
+const char	*Span::SpanFullException::what() const throw(){
+	return ("Span is already full");
+}
+
+const char	*Span::NotEnoughElementsExceptions::what() const throw(){
+	return ("Not enough elements in Span to compare");
+}
+
+//Member Functions
+
+void	Span::addNumber(int n)
+{
+	if (this->_elements.size() + 1 > this->_max)
+		throw (SpanFullException());
+	this->_elements.push_back(n);
+}
+
+void	Span::addNumber(std::vector<int>::iterator start, std::vector<int>::iterator end)
+{
+	if (this->_elements.size() + 1 > this->_max)
+		throw (SpanFullException());
+	this->_elements.insert(this->_elements.end(), start, end);
+}
+
+unsigned int	Span::shortestSpan(void)
+{
+	if (this->_elements.size() < 2)
+		throw (SpanFullException());
+	std::sort(this->_elements.begin(), this->_elements.end());
+	int	dif = 2147483647;
+	for (unsigned int i = 0; i < this->_max - 1; i++)
+		if (this->_elements[i + 1] - this->_elements[i] < dif)
+			dif = this->_elements[i + 1] - this->_elements[i];
+	return (dif);
+}
+
+unsigned int	Span::longestSpan(void)
+{
+	std::vector<int>::iterator	max;
+	std::vector<int>::iterator	min;
+
+	if (this->_elements.size() < 2)
+		throw (SpanFullException());
+	max = std::max_element(this->_elements.begin(), this->_elements.end());
+	min = std::min_element(this->_elements.begin(), this->_elements.end());
+	return (*max - *min);
+}
---
+++ ex01/Span.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 15:55:02 by andrferr          #+#    #+#             */
+/*   Updated: 2023/05/15 21:55:10 by andrferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class	Span
+{
+	public:
+		Span(unsigned int);
+		Span(const Span&);
+		Span	&operator=(const Span&);
+		~Span(void);
+
+		//Member Functions
+		void			addNumber(int);
+		void			addNumber(std::vector<int>::iterator start, std::vector<int>::iterator end);
+		unsigned int	shortestSpan(void);
+		unsigned int	longestSpan(void);
+
+		//Exceptions
+		class SpanFullException : std::exception{
+			public:
+				const char	*what() const throw();
+		};
+		class NotEnoughElementsExceptions : std::exception{
+			public:
+				const char	*what() const throw();
+		};
+
+	private:
+		unsigned int		_max;
+		std::vector<int>	_elements;
+};
+
+#endif
