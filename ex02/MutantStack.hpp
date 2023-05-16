2d19c7d7061a31c13624c991791736999dabab07
Andre Filipe Ferreira Miranda <andrferr@c2r5p13.hive.fi>
2023-05-16 11:39:06 +0300

    ex02 more tests added

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 08:26:05 by andrferr          #+#    #+#             */
-/*   Updated: 2023/05/16 09:46:38 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/05/16 11:15:07 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,18 @@
 #include <stack>
 #include <deque>
 
-template <typename T, typename Container = std::deque<T>>
+template <typename T, typename Container = std::deque<T> >
 class	MutantStack : public std::stack<T, Container>
 {
 	public:
 		MutantStack(void){};
-		MutantStack(const MutantStack&){};
-		MutantStack	&operator=(const MutantStack&){};
+		MutantStack(const MutantStack &other){
+			*this = other;
+		};
+		MutantStack	&operator=(const MutantStack &other){
+			this->c = other.c;
+			return (*this);
+		};
 		~MutantStack(void){};
 
 		//Iterator
